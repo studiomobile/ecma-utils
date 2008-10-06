0b2a998cfd8b6bbaa6720c6cb4763af9a2426df3
Andrey Verbin <averbin@fly.local>
2008-10-06 14:41:17 +0400

    script to create .tar.gz with project content

---
+++ makepackage.sh
@@ -0,0 +1,11 @@
+pkcgname=`echo $1 | sed "s/\/.*\///"`
+pkglocation=`echo $1 | sed "s/\(.*\/\).*/\1/"`
+if [ "$pkcgname" = "" ]; then
+	echo "Path to project folder should be without last slash"	
+	exit -1
+fi
+echo "$pkglocation"
+pushd .
+cd "$pkglocation"
+tar -czhf "$pkcgname.tar.gz" "$pkcgname"
+popd
