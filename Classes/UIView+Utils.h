cb7723f4a264b6db8e9d834f5c6aa4035bd5a72a
Ilya Romanov <ilya@Ilya.local>
2009-02-24 19:35:34 +0300

    Add missing files

---
+++ Classes/UIView+Utils.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+
+@interface UIView (utils)
+
+-(NSString*) hierarchyString;
+
+@end
---
+++ Classes/UIView+Utils.m
@@ -0,0 +1,23 @@
+#import "UIView+Utils.h"
+
+@implementation UIView (utils)
+
+-(NSMutableString*)addHierarchyTo: (NSMutableString*)hierarchyString prefix:(NSString*)prefix{
+	[hierarchyString appendFormat: @"%@%@ [%.0f %.0f %.0f %.0f]\n", prefix, self, 
+	 self.frame.origin.x, 
+	 self.frame.origin.y, 
+	 self.frame.size.width, 
+	 self.frame.size.height];
+	
+	for(UIView* v in self.subviews){
+		[v addHierarchyTo: hierarchyString prefix: [prefix stringByAppendingString:@"  "]];
+	}
+	
+	return hierarchyString;
+}
+
+-(NSString*) hierarchyString{
+	return [[[self addHierarchyTo: [NSMutableString string] prefix: @""] copy]autorelease];
+}
+
+@end
