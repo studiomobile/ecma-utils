5c4ec3d5ddcb14d8c91f7575d43f815cb06e3f29
Ilya Romanov <ilya@Ilya.local>
2009-04-17 23:25:38 +0400

    + SoapRequest

---
+++ Classes/Soap/SoapRequest.h
@@ -0,0 +1,34 @@
+#import "SoapEntityProto.h"
+
+@class SoapCustomEntity;
+
+@interface SoapRequest : NSObject {
+	NSString* url;
+	SoapCustomEntity* header;
+	id<SoapEntityProto> body;
+	id responseType;
+	BOOL responseIsMany;
+	NSArray* pathToResult;
+	id result;
+	NSError* error;
+}
+
+@property(retain) NSString* url;
+@property(retain) SoapCustomEntity* header;
+@property(retain) id<SoapEntityProto> body;
+@property(retain) id responseType;
+@property(assign) BOOL responseIsMany;
+@property(retain) NSArray* pathToResult;
+@property(retain, readonly)	id result; 
+@property(retain, readonly)	NSError* error; 
+
+-(BOOL)execute;
+
+@end
+
+@interface SoapRequest (Async)
+
+-(id)executeAndReturnResultOrError;
+
+@end
+
---
+++ Classes/Soap/SoapRequest.m
@@ -0,0 +1,93 @@
+#import "Soap.h"
+#import "SoapRequest.h"
+#import "RESTService.h"
+
+@interface SoapRequest ()
+
+@property(retain, readwrite)	id result; 
+@property(retain, readwrite)	NSError* error; 
+
+@end
+
+@implementation SoapRequest
+
+@synthesize header;
+@synthesize url;
+@synthesize body;
+@synthesize responseType;
+@synthesize responseIsMany;
+@synthesize pathToResult;
+@synthesize	result; 
+@synthesize	error; 
+
+-(void)dealloc{
+	[url release];
+	[header release];
+	[(NSObject*)body release];
+	[responseType release];
+	[pathToResult release];
+	[result release]; 
+	[error release]; 
+	
+	[super dealloc];
+}
+
+-(id)getResultFrom: (NSData*)data{	
+	NSString* responseString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]autorelease];
+	NSLog(@"SOAP RESPONSE:\n%@", responseString);	
+	SoapDeenveloper* deenveloper = [SoapDeenveloper soapDeenveloperWithXmlString:responseString];			
+	
+	if(responseIsMany){
+		return 	[deenveloper decodeBodyObjectsOfType: responseType];
+	}else{	
+		id res = [deenveloper decodeBodyObjectOfType: responseType];
+		if(pathToResult){
+			res = [res objectForPath: pathToResult];
+		}
+		return res;
+	}
+}
+
+
+-(BOOL)execute{	
+	self.error = nil;
+	self.result = nil;
+	
+	RESTService* service = [[[RESTService alloc]initWithBaseUrl:url]autorelease];	
+
+	NSDictionary* httpHeaders = [NSDictionary dictionaryWithObject:@"application/soap+xml; charset=utf-8" forKey:@"Content-Type"];
+	
+	SoapEnveloper* enveloper = [SoapEnveloper soapEnveloper];
+	if(header){
+		[enveloper encodeHeaderObject:header];	
+	}
+	[enveloper encodeBodyObject:body];	
+	NSString* xmlString = enveloper.message;
+	
+	NSData* requestData = [xmlString dataUsingEncoding: NSUTF8StringEncoding];
+	NSLog(@"SOAP REQUEST:\n%@", xmlString);
+	NSError* err = nil;
+	NSData* responseData = [service post:requestData to:@"" headers: httpHeaders error: &err];
+	if(err){
+		self.error = err;
+		return NO;
+	}		
+	
+	self.result = [self getResultFrom: responseData];
+	return YES;		
+}
+
+
+@end
+
+@implementation SoapRequest (Async)
+
+-(id)executeAndReturnResultOrError{
+	if([self execute]){
+		return self.result;
+	}else{
+		return self.error;
+	}	
+}
+
+@end
