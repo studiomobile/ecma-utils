c9e6f79b9be225c203ea58f01705d2712cbbffa5
hoha <hoha@hoha.local>
2009-05-12 16:47:56 +0400

    SwitchCell: sources cleaned up

--- Classes/Forms/SwitchCell.m
+++ Classes/Forms/SwitchCell.m
@@ -15,20 +15,24 @@
     return self;
 }
 
+
 - (void)dealloc {
 	[switcher release];	
     [super dealloc];
 }
 
+
 - (void)layoutControls:(CGRect)rect {
     switcher.frame = CGRectMake(rect.origin.x + rect.size.width - switcher.frame.size.width, rect.origin.y + (rect.size.height - switcher.frame.size.height)/2.0, switcher.frame.size.width, switcher.frame.size.height);
 }
+
 
 - (void)onFieldDescriptorUpdate {
     [super onFieldDescriptorUpdate];
     
 	self.switcher.on = [self.fieldDescriptor.value boolValue];
 }
+
 
 - (void)switched {
     self.fieldDescriptor.value = [NSNumber numberWithBool:switcher.on];
