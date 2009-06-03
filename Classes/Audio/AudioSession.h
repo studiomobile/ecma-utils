9aefb776b2cf87bea8f7abb757ff452bae841b40
Andrey Verbin <averbin@fly.local>
2009-06-03 20:24:32 +0400

    Improved AudioSession

--- Classes/Audio/AudioSession.h
+++ Classes/Audio/AudioSession.h
@@ -1,42 +1,3 @@
-/*
- 
- WisdomReader(tm) eBook Software
- 
- Version: 1.0
- 
- Copyright (c) 2009 Tree of Life Publishing Inc. All Rights Reserved.
- 
- Any redistribution, modification, or reproduction of part or all of the
- software or contents in any form is strictly prohibited without written
- permission by Tree of Life Publishing, Inc.
- 
- Do not make illegal copies of this software.
- 
- Contact:
- Tree of Life Publishing, Inc.
- 548 Market St. # 60253
- San Francisco, CA 94104
- www.WisdomTitles.com
- 
- IN NO EVENT SHALL TREE OF LIFE PUBLISHING, INC. BE LIABLE TO ANY PARTY FOR
- DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
- LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
- EVEN IF TREE OF LIFE PUBLISHING, INC. HAS BEEN ADVISED OF THE POSSIBILITY OF
- SUCH DAMAGE.
- 
- TREE OF LIFE PUBLISHING, INC. SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
- BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
- PROVIDED HEREUNDER IS PROVIDED "AS IS". TREE OF LIFE PUBLISHING, INC. HAS NO
- OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
- MODIFICATIONS.
- 
- */
-
-
-
-
-
 #import <UIKit/UIKit.h>
 #import "AudioPlayer.h"
 #import "AudioRecorder.h"
@@ -57,7 +18,7 @@ enum {
 + (BOOL)close;
 + (AudioSession*)session;
 
-- (void)activate;
+- (BOOL)activate:(UInt32)sessionCategory;
 - (AudioRecorder*)createRecorderForFile:(NSURL*)fileURL withFormat:(AudioStreamBasicDescription*)format;
 - (AudioPlayer*)createPlayerForFile:(NSURL*)fileURL;
 
--- Classes/Audio/AudioSession.m
+++ Classes/Audio/AudioSession.m
@@ -1,46 +1,6 @@
-/*
- 
- WisdomReader(tm) eBook Software
- 
- Version: 1.0
- 
- Copyright (c) 2009 Tree of Life Publishing Inc. All Rights Reserved.
- 
- Any redistribution, modification, or reproduction of part or all of the
- software or contents in any form is strictly prohibited without written
- permission by Tree of Life Publishing, Inc.
- 
- Do not make illegal copies of this software.
- 
- Contact:
- Tree of Life Publishing, Inc.
- 548 Market St. # 60253
- San Francisco, CA 94104
- www.WisdomTitles.com
- 
- IN NO EVENT SHALL TREE OF LIFE PUBLISHING, INC. BE LIABLE TO ANY PARTY FOR
- DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
- LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
- EVEN IF TREE OF LIFE PUBLISHING, INC. HAS BEEN ADVISED OF THE POSSIBILITY OF
- SUCH DAMAGE.
- 
- TREE OF LIFE PUBLISHING, INC. SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
- BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
- PROVIDED HEREUNDER IS PROVIDED "AS IS". TREE OF LIFE PUBLISHING, INC. HAS NO
- OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
- MODIFICATIONS.
- 
- */
-
-
-
-
-
 #import "AudioSession.h"
 
 static AudioSession *sessionStorage = nil;
-static BOOL inited = FALSE;
 
 @interface AudioSession ()
 
@@ -62,32 +22,23 @@ static void interruptHandler(void *data, UInt32  interruptionState) {
 @implementation AudioSession
 
 @synthesize interruptedOnPlayback;
-
+#define CHECK_ERROR(errMsg) if (status != noErr && status != kNotImplemented /*simulator return unimpErr*/)\
+        { NSLog(errMsg); [sessionStorage release]; sessionStorage = nil; return NO; }
 + (BOOL)open {
-    @synchronized(sessionStorage) {
-        if(sessionStorage == nil) {
-            OSStatus status = noErr;
-            if(!inited) {
-                sessionStorage = [[AudioSession alloc] init];
-                status = AudioSessionInitialize (NULL,
-                                                 NULL,
-                                                 interruptHandler,
-                                                 sessionStorage);
-                if(status == noErr || status == kNotImplemented /*simulator return unimpErr*/) {
-                    inited = TRUE;
-                }
-            }
-            if(status == noErr || status == kNotImplemented /*simulator return unimpErr*/) {
-                status = AudioSessionSetActive(TRUE);
-                if(status != noErr && status != kNotImplemented/*simulator return unimpErr*/) {
-                    NSLog(@"Failed to activate AudioSession: %d", status);
-                }
-                return YES;
-            }
+    OSStatus status = noErr;
+    @synchronized (sessionStorage) {
+        if (sessionStorage == nil) {
+            sessionStorage = [[AudioSession alloc] init];
+            status = AudioSessionInitialize (NULL,
+                                             NULL,
+                                             interruptHandler,
+                                             sessionStorage);
+            CHECK_ERROR(@"Failed to initialize audioSession");
         }
     }
-    return NO;
+    return status == noErr;
 }
+
 
 + (BOOL)close {
     @synchronized(sessionStorage) {
@@ -114,6 +65,7 @@ static void interruptHandler(void *data, UInt32  interruptionState) {
     [[NSNotificationCenter defaultCenter] postNotification:n];
 }
 
+
 - (void)onActivate {
     OSStatus status = AudioSessionSetActive(TRUE);
     if(status != noErr) {
@@ -124,11 +76,14 @@ static void interruptHandler(void *data, UInt32  interruptionState) {
     }
 }
 
-- (void)activate {
+
+- (BOOL)activate:(UInt32)sessionCategory {
+    AudioSessionSetProperty (kAudioSessionProperty_AudioCategory,
+                             sizeof (sessionCategory),
+                             &sessionCategory);
     OSStatus status = AudioSessionSetActive(TRUE);
-    if (status != noErr) {
-        NSLog(@"activate: Failed to activate AudioSesison");
-    }
+    CHECK_ERROR(@"Failed to activate AudioSesison");
+    return status == noErr;
 }
 
 
