990ba742882b42fb169b8cbe0d38e53f9f81c56c
serega <serega@serega.local>
2009-07-29 14:42:49 +0400

    add md5 hash calculation

--- Classes/NSData+MD5.m
+++ Classes/NSData+MD5.m
@@ -7,7 +7,9 @@
 - (NSString*)md5 {
     unsigned char md5[16];
     CC_MD5([self bytes], [self length], md5);
-    return [NSString stringWithCString:(char*)md5 length:16];
+	return [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            md5[0], md5[1], md5[2],  md5[3],  md5[4],  md5[5],  md5[6],  md5[7],
+            md5[8], md5[9], md5[10], md5[11], md5[12], md5[13], md5[14], md5[15]];
 }
 
 @end
