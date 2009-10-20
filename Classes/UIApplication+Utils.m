42b58d95da1fe7ec0d82b46ccb5c3afa67be8871
serega <serega@serega.local>
2009-10-20 13:37:47 +0400

    Bug #1130, Crash on adding new location

--- Classes/UIApplication+Utils.m
+++ Classes/UIApplication+Utils.m
@@ -12,7 +12,6 @@
 	UIApplication *app = [UIApplication sharedApplication];
 	UIWindow *window = app.keyWindow;
 	NSArray *subviews = [window subviews];
-	checkState([subviews count] == 1, @"Cannot find main view because window contain several views");
 	return [subviews objectAtIndex:0];
 }
 
