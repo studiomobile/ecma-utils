3e0421e3c05dfe7d54c8f2918b4efcffafe47c5b
Sergey <sergeytv@sergeytv.local>
2009-10-23 13:16:54 +0400

    webparams bug fix

--- Classes/Web/WebParams.m
+++ Classes/Web/WebParams.m
@@ -67,21 +67,25 @@
 	NSMutableData *postData = [NSMutableData data];
 	
     [postData appendData:[[NSString stringWithFormat:@"--%@\r\n", self.boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-	for (NSString *key in params) {
+    NSArray *keys = [params allKeys];
+	for (int i = 0; i < keys.count; ++i) {
+		NSString *key = [keys objectAtIndex:i];
 		NSObject *value = [params objectForKey:key];
 		if ([self isFileUpload:value]) {
             NSString *filename = [value respondsToSelector:@selector(filename)] ? [(id)value fileName] : key;
             NSData *data = [value respondsToSelector:@selector(data)] ? [(id)value data] : (NSData*)value;
-			[postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n\r\n", key, filename] dataUsingEncoding:NSUTF8StringEncoding]];
+			[postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\nContent-Type: image/jpeg\r\nContent-Transfer-Encoding: binary\r\n\r\n", key, filename] dataUsingEncoding:NSUTF8StringEncoding]];
 			[postData appendData:data];
 		} else {
 			[postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
 			[postData appendData:[[value description] dataUsingEncoding:NSUTF8StringEncoding]];
 		}
+		if (i == keys.count - 1) {
+			[postData appendData:[[NSString stringWithFormat:@"\r\n--%@--", self.boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+		} else {
+			[postData appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", self.boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+		}
 	}
-
-    [postData appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", self.boundary] dataUsingEncoding:NSUTF8StringEncoding]];
 	
 	return postData;
 }
