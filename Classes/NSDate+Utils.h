703ab3b5b4f867f7b4a33e375c1632becc1dd252
hoha <hoha@hoha.local>
2009-11-20 12:16:46 +0300

    NSDate+Utils: min & max added

--- Classes/NSDate+Utils.h
+++ Classes/NSDate+Utils.h
@@ -4,6 +4,8 @@
 
 - (BOOL)isAfter:(NSDate*)other;
 - (BOOL)isBefore:(NSDate*)other;
+- (NSDate*)minWith:(NSDate*)other;
+- (NSDate*)maxWith:(NSDate*)other;
 - (NSTimeInterval)minutesSinceDate:(NSDate*)date;
 - (NSDate*)addMinutes:(NSTimeInterval)minutes;
 - (NSTimeInterval)hoursSinceDate:(NSDate*)date;
--- Classes/NSDate+Utils.m
+++ Classes/NSDate+Utils.m
@@ -6,25 +6,41 @@
 	return [self compare:other] == NSOrderedDescending;
 }
 
+
 - (BOOL)isBefore:(NSDate*)other {
 	return [self compare:other] == NSOrderedAscending;
 }
+
+
+- (NSDate*)minWith:(NSDate*)other {
+    return [other isBefore:self] ? other : self;
+}
+
+
+- (NSDate*)maxWith:(NSDate*)other {
+    return [other isAfter:self] ? other : self;
+}
+
 
 - (NSTimeInterval)minutesSinceDate:(NSDate*)date {
 	return [self timeIntervalSinceDate:date]/60;
 }
 
+
 - (NSDate*)addMinutes:(NSTimeInterval)minutes {
 	return [self addTimeInterval:60*minutes];
 }
+
 
 - (NSTimeInterval)hoursSinceDate:(NSDate*)date {
 	return [self timeIntervalSinceDate:date]/3600;
 }
 
+
 - (NSTimeInterval)daysSinceDate: (NSDate*)date{
 	return [self hoursSinceDate:date]/24;
 }
+
 
 - (NSDate*)dayStart {
 	NSCalendar *calendar = [NSCalendar currentCalendar];
