79e6cffdb27449936612a9efa6a1c28d8829c55b
hoha <hoha@hoha.local>
2009-12-03 19:02:33 +0300

    UIViewUtils: hierarchy string changed

--- Classes/UIView+Utils.m
+++ Classes/UIView+Utils.m
@@ -3,11 +3,7 @@
 @implementation UIView(utils)
 
 - (NSMutableString*)addHierarchyTo:(NSMutableString*)hierarchyString prefix:(NSString*)prefix {
-	[hierarchyString appendFormat:@"%@%@ [%.0f %.0f %.0f %.0f]\n", prefix, self, 
-	 self.frame.origin.x, 
-	 self.frame.origin.y, 
-	 self.frame.size.width, 
-	 self.frame.size.height];
+	[hierarchyString appendFormat:@"%@%@ [%d]\n", prefix, self, self.tag];
 	
 	for(UIView *v in self.subviews){
 		[v addHierarchyTo:hierarchyString prefix:[prefix stringByAppendingString:@"  "]];
