a07ad0d923f6b5a6af1b9b248c934819e64a6120
Ilya Romanov <ilya@ilya.local>
2010-01-17 23:53:13 +0300

    callback arguments binding

--- Classes/Async/Callback.h
+++ Classes/Async/Callback.h
@@ -1,7 +1,8 @@
 @interface Callback : NSObject {
     id handler;
 	BOOL isHandlerRetained;
-    SEL selector;	
+    SEL selector;
+	NSMutableDictionary* boundArguments;
 }
 @property(assign) id delegate;
 @property(retain) id observer;
@@ -10,10 +11,11 @@
 +(Callback*) callbackWithDelegate: delegate selector: (SEL)selector;
 +(Callback*) callbackWithObserver: observer selector: (SEL)selector;
 
-
 -(void)call;
 -(void)callWith: arg;
 -(void)callWith: arg1 with: arg2;
 -(void)callWithArguments: (NSArray*)args;
+
+-(void)bindArgument: (int)number with: (id)value;
 
 @end
--- Classes/Async/Callback.m
+++ Classes/Async/Callback.m
@@ -4,6 +4,42 @@
 @implementation Callback
 @synthesize selector;
 
+#pragma mark private
+
+// construct bound arguments array
+// fill the gaps with passed args
+-(NSArray*)finalArgumentsWith: (NSArray*)args{	
+	NSMutableArray* result = [NSMutableArray array];
+	NSMutableArray* callArgsReversed = [[[args reverseObjectEnumerator] allObjects] mutableCopy];
+	NSArray* boundNumbers = [boundArguments allKeys];
+	int max = [[boundNumbers valueForKeyPath: @"@max.intValue"] intValue];
+	for(int i = 0; i <= max; i++){
+		NSNumber* key = [NSNumber numberWithInt: i];
+		id val = [boundArguments objectForKey: key];
+		if(val){
+			[result addObject: val];			
+		}else if(callArgsReversed.count){			
+			[result addObject: [callArgsReversed lastObject]];
+			[callArgsReversed removeLastObject];
+		}else {
+			[result addObject: [NSNull null]];
+		}
+	}
+
+	while (callArgsReversed.count) {
+		[result addObject: [callArgsReversed lastObject]];
+		[callArgsReversed removeLastObject];
+	}
+	
+	[callArgsReversed release];
+	
+	return [[result copy] autorelease];
+}
+
+-(id) convertNull: arg{
+	return arg ? arg : [NSNull null];
+}
+
 #pragma mark properties
 
 -(id)delegate{
@@ -35,6 +71,8 @@
 			selector: (SEL)_selector {
 	self = [super init];
 	if (self != nil) {
+		boundArguments = [[NSMutableDictionary dictionary] retain];
+		
 		if(isHandlerRetained) self.observer = _handler;
 		else self.delegate = _handler;
 		self.selector = _selector;
@@ -45,6 +83,7 @@
 
 
 - (void) dealloc{
+	[boundArguments release];
 	if(isHandlerRetained) [handler release];
 	[super dealloc];
 }
@@ -60,11 +99,14 @@
 }
 
 -(void)callWithArguments: (NSArray*)args{
+	args = [self finalArgumentsWith: args];
+	
 	NSInvocation* inv = [handler invocationForMethod: selector];
 	for(int i = 2; i < inv.methodSignature.numberOfArguments; i++){
 		int argN = i - 2;
 		id arg = argN < args.count ? [args objectAtIndex: argN] : nil;
-		[inv setArgument:  &arg atIndex:i];
+		if(arg == [NSNull null]) arg = nil;
+		[inv setArgument: &arg atIndex:i];
 	}
 
 	[inv invoke];
@@ -75,11 +117,19 @@
 }
 
 -(void)callWith: arg{	
+	arg = [self convertNull:arg];	
 	[self callWithArguments: [NSArray arrayWithObject: arg]];
 }
 
 -(void)callWith: arg1 with: arg2{
+	arg1 = [self convertNull:arg1];
+	arg2 = [self convertNull:arg2];
 	[self callWithArguments: [NSArray arrayWithObjects: arg1, arg2, nil]];
+}
+
+-(void)bindArgument: (int)number with: (id)value{
+	value = [self convertNull: value];
+	[boundArguments setObject: value forKey: [NSNumber numberWithInt: number]];
 }
 
 
